32c4b9f69af29035a1c5bcfbb3f1a2d6802c86cd
maxim payeur <maxim.payeur@gmail.com>
2022-06-20 12:06:39 +0200

    Add project files.

---
+++ Service/CalculationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingApp.Service
+{
+    public class CalculationService
+    {
+        public int TheSumOfTwoNumbers(int num1, int num2) {
+            return num1 + num2;
+        }
+
+        public int SubstractTwoNumbers(int num1, int num2) { 
+            return num1 - num2;
+        }
+            
+    }
+}
